79430a60a0d01ee349f3b2fc29b7ed8e1bba54c4
Stuart Pook <s.pook@criteo.com>
2016-12-12 18:13:19 +0100

    when quiet set ringer to full volume

--- app/src/main/java/it/pook/setdonotdisturb/NotificationFilterChangerService.java
+++ app/src/main/java/it/pook/setdonotdisturb/NotificationFilterChangerService.java
@@ -19,6 +19,7 @@ package it.pook.setdonotdisturb;
 import android.app.IntentService;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Build;
 import android.util.Log;
 import android.content.Context;
@@ -50,7 +51,8 @@ public class NotificationFilterChangerService extends IntentService {
     @Override
     protected void onHandleIntent(Intent in) {
         Log.i(TAG, ".onHandleIntent " + in);
-        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        final Context context = getApplicationContext();
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             /*if (!notificationManager.isNotificationPolicyAccessGranted()) {
@@ -64,6 +66,10 @@ public class NotificationFilterChangerService extends IntentService {
                     boolean quiet = in.getBooleanExtra("quiet", true);
                     filter = quiet ? NotificationManager.INTERRUPTION_FILTER_PRIORITY : NotificationManager.INTERRUPTION_FILTER_ALL;
                     notificationManager.setInterruptionFilter(filter);
+                    AudioManager mobilemode = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+                    int volume = in.getIntExtra("ringer", mobilemode.getStreamMaxVolume(AudioManager.STREAM_RING));
+                    Log.i(TAG, ".onHandleIntent volume " + volume);
+                    mobilemode.setStreamVolume(AudioManager.STREAM_RING, volume, 0);
                 } else {
                     Log.i(TAG, ".onHandleIntent mode " + mode);
                     if (mode.equals("all"))
